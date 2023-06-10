5973c5e3f0c8b2ec8ba854dd91557cb0fc158755
Dipti Patel <diptipatel3oct2001@gmail.com>
2023-06-10 19:08:26 +0530

    Changes in app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const lodash = require("lodash");
 const mongoose = require("mongoose");
+const mongodb=require("mongodb")
 
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -30,7 +31,7 @@ const Blog = mongoose.model("Blog", blogSchema);
 app.get("/", function (req, res) {
   Blog.find({}, function (err, foundList) {
     if (!err) {
-      console.log(foundList);
+      // console.log(foundList);
       res.render("home", { startingContent: homeStartingContent, list: foundList });
     }
     else {
@@ -72,6 +73,19 @@ res.redirect("/");
   });
 })
 
+const deleteBlog=()=>{
+
+}
+const clicked=()=>{
+  console.log("CLEICKED---------------------------------------------------------------------------------")
+}
+
+app.post("/delete/:postId",async function(req,res){
+  let postId = req.params.postId;
+  console.log(postId);
+  const result=await Blog.deleteOne({_id:req.params.postId})
+  res.redirect("/")
+})
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
